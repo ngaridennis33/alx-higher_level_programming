0332700b7a3d6ea684b3160cb0e4861659f9da27
Dennis Ngari <ngaridennis33@gmail.com>
2022-12-13 13:32:35 -0800

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Dennis Ngari
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
