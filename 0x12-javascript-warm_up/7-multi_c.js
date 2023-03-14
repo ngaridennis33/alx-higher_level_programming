97e51f7a13f4e39c4b38ad620369e1d2bc305bb8
Dennis <dennisngari3@gmail.com>
2023-03-14 01:13:16 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
 let txt = "C is fun";
-let result = process.argv[2];
-for (let i = 0; i < result; i++) {
-  console.log(txt);
+let result = parseInt(process.argv[2]);
+if (!isNaN(result)) {
+  for (let i = 0; i < result; i++) {
+    console.log(txt);
+  }
+} else {
+  console.log("Input missing or NaN");
 }
