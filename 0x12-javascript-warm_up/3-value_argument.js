7beed86e3c660a2d07cf175a9cda8f58cd7866a4
Dennis <dennisngari3@gmail.com>
2023-03-14 00:13:35 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
+
 #!/usr/bin/node
 
-if (!process.argv[2]) {
-  console.log("No argument");
-} else if (process.argv[2]) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
