490e1806a2ad8b111a542e05a3b6917c2abe82a0
Dennis Ngari <ngaridennis33@gmail.com>
2023-04-06 11:50:24 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-# Script that fetches a URL using urlib
-from urllib import request
+"""A python script that
+- fetches  a url using urllib package
+"""
+from urllib import request, error
+
 if __name__ == "__main__":
+    try:
         with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
             html = response.read()
             print(f"Body response:\n\t- type: {type(html)}\n\t- content: {html}\n\t- utf8 content: {html.decode()}")
+    except error.URLError as e:
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
