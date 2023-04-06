a996b37f2fa547c3e33de390e59df5069345254d
Dennis Ngari <ngaridennis33@gmail.com>
2023-04-06 10:48:47 -0700

    Added a python script that fetches a URL using urllib package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,12 @@
 
 from urllib import request, error
 
-    
-try:
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(html)}\n\t- content: {html}\n\t- utf8 content: {html.decode()}")
-except error.URLError as e:
-    print('The server couldn\'t fulfill the request.')
-    print('Error code: ', e.code)
+if __name__ == "__main__":
+    try:
+        with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+            html = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(html)}\n\t- content: {html}\n\t- utf8 content: {html.decode()}")
+    except error.URLError as e:
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
